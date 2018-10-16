36886ad6398b650273268bc3ef8b19574cb081ca
Yi Zhao <yizhao@Yis-MacBook-Pro.lan>
2018-10-16 22:17:47 +1300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Card from "./component/Card";
 
 class App extends Component {
+  state = {
+    data: []
+  };
+
+  componentDidMount() {
+    const url = "https://jsonplaceholder.typicode.com/users";
+
+    fetch(url) // Call the fetch function passing the url of the API as a parameter
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          data: json
+        });
+      })
+      .catch(function() {
+        // This is where you run code if the server returns any errors
+      });
+  }
+
+  handleDelete = user => {
+    let clonedUsers = [...this.state.data];
+    clonedUsers = clonedUsers.filter(item => item !== user);
+
+    this.setState({
+      data: clonedUsers
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>test</h1>
+        {this.state.data.map(user => (
+          <Card key={user.id} user={user} onDelete={this.handleDelete} />
+        ))}
       </div>
     );
   }
---
+++ src/component/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card = props => {
+  const { user, onDelete } = props;
+  return (
+    <div className="card" key={user.id}>
+      <h3 className="card-title">{user.name}</h3>
+      <p>
+        <a href={"mailto:" + user.email}>{user.email}</a>
+      </p>
+      <button onClick={() => onDelete(user)}>Delete</button>
+    </div>
+  );
+};
+
+export default Card;
